fac34af14aa151a3cfd4f513d7707db663425b57
Lukas <sivaklukas@yahoo.com>
2025-04-22 16:16:22 +0100

    commit

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+const carouselImages = document.querySelectorAll('.carousel-img');
+const prevBtn = document.querySelector('.carousel-btn.prev');
+const nextBtn = document.querySelector('.carousel-btn.next');
+
+let currentIndex = 0;
+
+function showImage(index) {
+  carouselImages.forEach((img, i) => {
+    img.classList.remove('active');
+    if (i === index) img.classList.add('active');
+  });
+}
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + carouselImages.length) % carouselImages.length;
+  showImage(currentIndex);
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % carouselImages.length;
+  showImage(currentIndex);
+});
+
+showImage(currentIndex);
